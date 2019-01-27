4f4f8fc9ab795821d796e23cfdcd5e7b983b5ef3
Jayant Sinha <jayant.sinha@hotmail.com>
2019-01-28 03:49:05 +0530

    Corrected error text and fmt

--- Errors.go
+++ Errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ParkingLotNotCreated = errors.New("Parking lot not created")
-	UnableToPark = errors.New("Unable to park. Please create a parking lot first.")
-	ParkingLotFull = errors.New("Parking lot is full. Unable to park.")
-	UnableToVacate = errors.New("Unable to vacate the slot")
+	UnableToPark         = errors.New("Unable to park. Please create a parking lot first.")
+	ParkingLotFull       = errors.New("Parking lot is full. Unable to park.")
+	UnableToVacate       = errors.New("Slot already empty; unable to vacate.")
 )
