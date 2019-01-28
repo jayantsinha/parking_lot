19edf5a42ff744b882d06009efb8c710386d73b8
Jayant Sinha <jayant.sinha@hotmail.com>
2019-01-28 13:20:48 +0530

    Added error for Leave() call on invalid parking slot

--- Errors.go
+++ Errors.go
@@ -3,8 +3,9 @@ package main
 import "errors"
 
 var (
-	ParkingLotNotCreated = errors.New("Parking lot not created")
-	UnableToPark         = errors.New("Unable to park. Please create a parking lot first.")
-	ParkingLotFull       = errors.New("Parking lot is full. Unable to park.")
-	UnableToVacate       = errors.New("Slot already empty; unable to vacate.")
+	ParkingLotNotCreated            = errors.New("Parking lot not created")
+	UnableToPark                    = errors.New("Unable to park. Please create a parking lot first.")
+	ParkingLotFull                  = errors.New("Parking lot is full. Unable to park.")
+	UnableToVacateEmptySlot         = errors.New("Slot already empty; unable to vacate.")
+	UnableToVacateOnNonExistentSlot = errors.New("Slot not found; unable to vacate.")
 )
