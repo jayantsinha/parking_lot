dfbf75ea002f330ec03a73d053a9dfb7fb060992
Jayant Sinha <jayant.sinha@hotmail.com>
2019-01-29 01:49:03 +0530

    Added var for `version` command

--- commands.go
+++ commands.go
@@ -9,5 +9,5 @@ var (
 	CmdSlotNumsForCarsWithColor = "slot_numbers_for_cars_with_colour"
 	CmdSlotNumForRegNum         = "slot_number_for_registration_number"
 	CmdExit                     = "exit"
+	CmdVersion					= "version"
 )
-
