ce4b1b700ace1d20a9b94841f890c1bd7f8faa2e
Jayant Sinha <jayant.sinha@hotmail.com>
2019-01-29 02:16:08 +0530

    Implemented command exectution by file

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main() {
+var singleCmds map[string]bool
+var funcMapper map[string]func([]string) bool
 
+func init() {
 	flag.Parse()
 
-	funcMapper := map[string]func([]string) bool{
+	funcMapper = map[string]func([]string) bool{
 		CmdCreateParkingLot:         CreateParkingLot,
 		CmdPark:                     ParkVehicle,
 		CmdLeave:                    LeaveSlot,
@@ -20,15 +25,17 @@ func main() {
 		CmdExit:                     EndSession,
 		CmdVersion:                  ShowVersion,
 	}
-	singleCmds := map[string]bool{CmdStatus: true, CmdExit: true, CmdVersion: true}
+	singleCmds = map[string]bool{CmdStatus: true, CmdExit: true, CmdVersion: true}
+}
 
+func main() {
 	if len(flag.Args()) < 1 {
 		fmt.Println("Invalid command")
 	}
 
 	_, isSingleCmd := singleCmds[flag.Args()[0]]
 	if len(flag.Args()) == 1  &&  !isSingleCmd {
-		ExecFile(flag.Args()[0])
+		execFile(flag.Args()[0])
 	}
 
 	if len(flag.Args()) > 0 {
@@ -38,5 +45,49 @@ func main() {
 			return
 		}
 		CallRespFunc(flag.Args(), funcMapper[flag.Args()[0]])
+	}
+}
+
+// execFile executes commands from a text file
+func execFile(filepath string) {
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		_ = fmt.Errorf("Error encountered while reading file: Err: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		args := separateFlags(line)
+		processArgs(args)
+	}
+}
+
+func separateFlags(line string) (args []string) {
+	line = strings.TrimSpace(line)
+	args = strings.Split(line, " ")
+	return
+}
+
+func processArgs(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Invalid command")
+	}
+
+	_, isSingleCmd := singleCmds[args[0]]
+	if len(args) == 1  &&  !isSingleCmd {
+		execFile(args[0])
+	}
+
+	if len(args) > 0 {
+		_, isValidCmd := funcMapper[args[0]]
+		if !isValidCmd {
+			fmt.Println("Unknown command")
+			return
+		}
+		CallRespFunc(args, funcMapper[args[0]])
 	}
 }
