b4ac82dbed459638cc63019f0898d18825e4a505
Jayant Sinha <jayant.sinha@hotmail.com>
2019-01-28 17:06:14 +0530

    Added errors for invalid input and empty parking lot

--- Errors.go
+++ Errors.go
@@ -9,4 +9,6 @@ var (
 	UnableToVacateEmptySlot         = errors.New("Slot already empty; unable to vacate.")
 	UnableToVacateOnNonExistentSlot = errors.New("Slot not found; unable to vacate.")
 	NotFound                        = errors.New("Not Found")
+	InvalidInput                    = errors.New("Invalid Input")
+	ParkingLotEmpty                 = errors.New("Parking lot empty!")
 )
