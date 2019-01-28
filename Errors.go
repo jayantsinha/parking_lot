71cfa57b2ae3aa4735d4cf245ecdb327d4a37fbe
Jayant Sinha <jayant.sinha@hotmail.com>
2019-01-29 04:41:36 +0530

    Fixed file name

--- Errors.go
+++ Errors.go
@@ -5,7 +5,7 @@ import "errors"
 var (
 	ParkingLotNotCreated            = errors.New("Parking lot not created")
 	UnableToPark                    = errors.New("Unable to park. Please create a parking lot first.")
-	ParkingLotFull                  = errors.New("Parking lot is full. Unable to park.")
+	ParkingLotFull                  = errors.New("Sorry, parking lot is full")
 	UnableToVacateEmptySlot         = errors.New("Slot already empty; unable to vacate.")
 	UnableToVacateOnNonExistentSlot = errors.New("Slot not found; unable to vacate.")
 	NotFound                        = errors.New("Not Found")
