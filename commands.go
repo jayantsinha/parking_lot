05cfe30d5cc067958e4a5fad6276fcc8f249837d
Jayant Sinha <jayant.sinha@hotmail.com>
2019-01-28 23:02:54 +0530

    Added vars to store all the cli commands

---
+++ commands.go
@@ -0,0 +1,13 @@
+package main
+
+var (
+	CmdCreateParkingLot         = "create_parking_lot"
+	CmdPark                     = "park"
+	CmdLeave                    = "leave"
+	CmdStatus                   = "status"
+	CmdRegNumsForCarsWithColor  = "registration_numbers_for_cars_with_colour"
+	CmdSlotNumsForCarsWithColor = "slot_numbers_for_cars_with_colour"
+	CmdSlotNumForRegNum         = "slot_number_for_registration_number"
+	CmdExit                     = "exit"
+)
+
